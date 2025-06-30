8d36cd5ac1a989db98c229cd76986aacd5e41e98
nativa596 <freefiretable@gmail.com>
2025-06-30 10:28:07 -0300

    updating project files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,54 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+
+import { useState } from 'react';
+import { Eye, EyeSlash } from 'phosphor-react';
+import Link from "next/link";
+import styles from './page.module.css';
 
 export default function Home() {
+  const [ showPassword, setShowPassword ] = useState(false);
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      <div className={styles.page}>
+        <main className={styles.title}>
+          <h1>Login | ConnectAI</h1>
+        </main>
+        <div className={styles.divEmail}>
+          <label className={styles.labelAnim}> 
+          <input
+            type="email"
+            id="loginEmail"
+            placeholder=""
+            required
+          />
+          <span>Your Email or Username</span>
+          </label>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+        <div className={styles.divPassword}>
+          <label className={styles.labelAnim}>
+          <input
+            type={showPassword ? 'text' : 'password' }
+            id="loginPassword"
+            placeholder=""
+            required
           />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+          <span>Your Password</span>
+          </label>
+          <button
+          type="button"
+          onClick={() => setShowPassword(!showPassword) }
+          onMouseDown={(e) => e.preventDefault()}
+          >
+            { showPassword ? <EyeSlash size={24} /> : <Eye size={24} /> }
+          </button>
+         </div>
+         <div className={styles.buttonSubmit}>
+          <button
+            type="submit"
+          >Login</button>
+        </div>
+         <div className={styles.linkDiv}>
+          <Link href="/register">Dont have an account? Go to the registration page.</Link>
+         </div>
+      </div>
+  )
 }
